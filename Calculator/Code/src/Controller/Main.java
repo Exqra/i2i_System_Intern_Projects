279c9c3ee89abbe3ec8b09358c4637dec39cad71
Tolga Çatalpınar <34005953+Exqra@users.noreply.github.com>
2019-06-27 10:17:23 +0300

    Add files via upload

---
+++ Calculator/Code/src/Controller/Main.java
@@ -0,0 +1,24 @@
+package Controller;
+
+
+public class Main {
+	
+	public static double sum( double a, double b)
+	{
+		return a + b;
+	}
+	public static double subtract( double a, double b)
+	{
+		return a - b;
+	}
+	
+	public static double multiply( double a, double b)
+	{
+		return a * b;
+	}
+	public static double divide( double a, double b)
+	{
+		return a / b;
+	}
+
+}
---
+++ Calculator/Code/src/Controller/MainController.java
@@ -0,0 +1,30 @@
+package Controller;
+
+
+
+public class MainController {
+	
+
+	
+	private static double sum( double a, double b)
+	{
+		return a + b;
+	}
+	private static double subtract( double a, double b)
+	{
+		return a - b;
+	}
+	
+	private static double multiply( double a, double b)
+	{
+		return a * b;
+	}
+	private static double divide( double a, double b)
+	{
+		return a / b;
+	}
+	
+	
+	
+
+}
