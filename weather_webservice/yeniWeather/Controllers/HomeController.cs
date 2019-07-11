ce56a7c05caf14d04700fc140e37d9122820df70
Tolga Çatalpınar <34005953+Exqra@users.noreply.github.com>
2019-07-11 17:29:30 +0300

    Add files via upload

---
+++ weather_webservice/yeniWeather/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+using yeniWeather.Models;
+
+namespace yeniWeather.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult HavaDurumu()
+        {
+            string api = "&APPID=da0c7a97480955692dbfc4a00e229641";
+            string baglanti = "https://api.openweathermap.org/data/2.5/weather?q=London&mode=xml" + api;
+            XDocument Hava = XDocument.Load(baglanti);
+            var sicaklik = Hava.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            var icon = Hava.Descendants("weather").ElementAt(0).Attribute("icon").Value;
+            var durum = Hava.Descendants("weather").ElementAt(0).Attribute("value").Value;
+            ViewBag.icon = "http://openweathermap.org/img/w/" + icon + ".png";
+            ViewBag.sicaklik = sicaklik + " ºC";
+            ViewBag.durum = durum;
+
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
